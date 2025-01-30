0e9451ab4e823ddb1853e9caac1623019b272866
Juan <jjramirezg09@gmail.com>
2025-01-30 02:09:53 -0500

    connection mysql

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"github.com/Cococtel/Cococtel_BaBackend/internal/http"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
@@ -25,11 +24,6 @@ func main() {
 	}
 }
 func createDB() (*sql.DB, error) {
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbName := os.Getenv("DB_NAME")
-	var connectionString = fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&tls=true", dbUser, dbPassword, dbHost, dbName)
-
-	return sql.Open("mysql", connectionString)
+	dns := os.Getenv("DB_DNS")
+	return sql.Open("mysql", dns)
 }
