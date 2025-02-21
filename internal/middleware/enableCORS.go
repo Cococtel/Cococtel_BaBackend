1086156ff782be3961050b1c6b59ac68d42169cd
Juan <jjramirezg09@gmail.com>
2025-02-20 22:11:47 -0500

    add header

--- internal/middleware/enableCORS.go
+++ internal/middleware/enableCORS.go
@@ -9,7 +9,7 @@ func CORS() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		ctx.Writer.Header().Add("Access-Control-Allow-Origin", "*")
 		ctx.Writer.Header().Add("Access-Control-Allow-Credentials", "true")
-		ctx.Writer.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		ctx.Writer.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, x-auth-token")
 		ctx.Writer.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
 		if ctx.Request.Method == "OPTIONS" {
